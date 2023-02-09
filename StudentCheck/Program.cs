b219ea9868a3d65ea06e8c360e8cae511f7b67b1
David Chuvik <dchuvik@gmail.com>
2023-02-09 12:18:22 -0500

    test

---
+++ StudentCheck/Program.cs
@@ -0,0 +1,31 @@
+﻿
+
+
+
+
+Console.Write("What is your name: ");
+string firstName = Console.ReadLine();
+
+Console.Write("What is your age: ");
+bool isValidInt = int.TryParse(Console.ReadLine(), out int age);
+
+if (firstName.ToLower() == "bob" || firstName.ToLower() == "sue")
+{
+    Console.WriteLine("Hello Professor");
+}
+else
+{
+    Console.WriteLine("I don't know who you are.");
+    if (age < 21)
+    {
+        Console.WriteLine($"you are recommended to wait {21 - age} years," +
+            $" until you are 21, to start this class");
+    }
+    else
+    {
+        Console.WriteLine("welcome to class");
+    }
+
+}
+
+
